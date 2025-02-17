a54c369f160dadec24bad69ea102b80d76902d43
ekladiousm <mira.ekladious@taico.io>
2025-02-17 16:58:08 +0100

    added report creation stream

--- airbyte-integrations/connectors/source-adform/source_adform/source.py
+++ airbyte-integrations/connectors/source-adform/source_adform/source.py
@@ -35,118 +35,127 @@ https://docs.airbyte.com/connector-development/
 
 import time
 import requests
+from datetime import date
+
+from typing import Any, Iterable, List, Mapping, Optional, Tuple, Union
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
+from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
+DATE_FORMAT = "%Y-%m-%d"
+POLLING_IN_SECONDS = 30
+DEFAULT_START_DATE = "2016-01-01"
 
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
+class ReportCreationStream(HttpStream):
+    # Implementation for creating the report and getting the report ID
+    # ...
+    http_method = "POST"
+    url_base = "https://api.adform.com"
+    page_size = 100
+    # max_pages = 100
+    
+    def __init__(self, authenticator, name, path, config, primary_key=None):
+        self._name = name
+        self._path = path
+        self.config = config
+        self._primary_key = primary_key
+        self._offset = 0
+        self._total_rows = None
+        self._current_report_id = None
+        super().__init__(authenticator=authenticator)
+    
+    @property
+    def name(self) -> str:
+        return self._name
 
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
-
-# Basic full refresh stream
-class AdformStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class AdformStream(HttpStream, ABC)` which is the current class
-    `class Customers(AdformStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(AdformStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalAdformStream((AdformStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
-
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    @property
+    def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
+        return self._primary_key
+    
+    def stream_slices(self, sync_mode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None) -> Iterable[Optional[Mapping[str, Any]]]:
+        self.logger.info(f"Generating stream slice with offset: {self._offset}")
+        yield {"offset": self._offset}
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
         return None
-
-    def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
-        return {}
-
+    
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
-        """
-        yield {}
-
-
-class Customers(AdformStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+        headers = response.headers
+        self._current_report_id = headers['Location']
+        self.logger.info(f"Parsed response, got report_path: {self._current_report_id}")
+        yield {'report_path': self._current_report_id, 'offset': self._offset}
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        *,
+        stream_state: Optional[Mapping[str, Any]] = None,
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
     ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "customers"
+        return self._path
+    
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        today: date = date.today()
 
+        start_date = self.config.get("start_date", today.strftime(DATE_FORMAT))
+        end_date = self.config.get("end_date", DEFAULT_START_DATE) 
 
-# Basic incremental stream
-class IncrementalAdformStream(AdformStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
+        request_payload = {
+            "filter": {
+                "date": {
+                    "From": start_date,
+                    "To": end_date
+                }
+            },
+            "metrics": self.config["metrics"],
+            "dimensions": self.config["dimensions"],
+            "paging": {
+                "offset": self._offset,
+                "limit": self.page_size,
+            },
+            "includeRowCount": True
+        }
+        return request_payload
+    
+    def backoff_time(self, response: requests.Response) -> Optional[float]:
+        if isinstance(response, Exception):
+            self.logger.warning(f"Back off due to {type(response)}.")
+            return POLLING_IN_SECONDS * 2
+        if response.status_code == 429:
+            self.logger.warning(f"API calls quota exceeded, maximum admitted 10 per minute")
+            return POLLING_IN_SECONDS * 2
+        return super().backoff_time(response)
+    
+    def should_retry(self, response: requests.Response) -> bool:
+        return response.status_code in [429]
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+    def get_json_schema(self):
+        return {
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "type": "object",
+            "properties": {
+                "report_path": {
+                    "type": "string"
+                }
+            }
+        }
+
+class ReportDataStream(HttpSubStream):
+    http_method = "GET"
+    url_base = "https://api.adform.com"
+    def __init__(self, name, authenticator, parent, config, primary_key = None):
+        self._name = name
+        self.config = config
+        self._primary_key = primary_key
+        self._has_more_data = True
+        super().__init__(parent=parent, authenticator=authenticator)
 
     @property
     def cursor_field(self) -> str:
